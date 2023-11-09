56e798ca6d71aefc5229cfcdef57dd627385162c
Andy Li <117238442+liandy300@users.noreply.github.com>
2023-11-09 17:42:25 -0500

    collision_2
    Wrote down task that collision file will complete

--- Game_Code/Game_Code.srcs/sources_1/new/Main.vhd
+++ Game_Code/Game_Code.srcs/sources_1/new/Main.vhd
@@ -11,16 +11,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Main is
--- 14x14 Playable grid
--- Take in 15 vectors 1 player, 1 coin, 13 projectiles
---  Port ( ); 
+-- Total Grid: 16x16
+-- 14x14 Playable grid (1 to 14), Row and Column 0 and 15 are unplayable(outside grid).
+-- Take in 15 vectors, 1 player, 1 coin, 13 projectiles.
+-- Game with max at 13 projectiles.
+-- Keep a life counter and score counter. Use seven seg on FPGA to display lives and score.
+-- Left seven seg used for lives, Right two seven seg used for score.  
+-- Port ( ); 
 
 end Main;
 
 architecture Behavioral of Main is
-    -- Check for Collisions, player position with respect to all projectile, minus one life if no more lifes end game
-    --                       player position with coin, plus one score and activate another projectile
-    --                       projectile with coin, skip over
+    -- Check for Collisions, player position with respect to all projectile, minus one life if no more lifes end game, player and projectile stays moving in same position
+    --                       player position with coin, plus one score and activate another projectile and setout signal to reset coin position player keeps moving 
+    --                       projectile with coin, skip over - do nothing
     -- Once detected a collision, send out a T or F boolean
 begin
 
