f912a4243f0bc780edddc9b349e5472ae3feda4b
Andy Li <117238442+liandy300@users.noreply.github.com>
2023-11-09 17:45:04 -0500

    update

--- Game_Code/Game_Code.srcs/sources_1/new/Collision.vhd
+++ Game_Code/Game_Code.srcs/sources_1/new/Collision.vhd
@@ -32,12 +32,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Collision is
---  Port ( );
+-- Total Grid: 16x16
+-- 14x14 Playable grid (1 to 14), Row and Column 0 and 15 are unplayable(outside grid).
+-- Take in 15 vectors, 1 player, 1 coin, 13 projectiles.
+-- Game with max at 13 projectiles.
+-- Keep a life counter and score counter. Use seven seg on FPGA to display lives and score.
+-- Left seven seg used for lives, Right two seven seg used for score.  
+-- Port ( ); 
+
 end Collision;
 
 architecture Behavioral of Collision is
-
+    -- Check for Collisions, player position with respect to all projectile, minus one life if no more lifes end game, player and projectile stays moving in same position
+    --                       player position with coin, plus one score and activate another projectile and setout signal to reset coin position player keeps moving 
+    --                       projectile with coin, skip over - do nothing
+    -- Once detected a collision, send out a T or F boolean
 begin
+
+
 
 
 end Behavioral;
